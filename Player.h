6f42ea23638b144d24e0346772c0d7d3779369b6
smeridian387 <43002188+smeridian387@users.noreply.github.com>
2018-10-01 16:56:29 +0100

    playerHeader

--- Player.h
+++ Player.h
@@ -1,1 +1,29 @@
 #pragma once
+
+//library includes
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+//project include
+#include "AnimationSystem.h"
+class Player
+{
+public:
+
+	Player();
+
+	void Input(sf::Event _gameEvent);
+	void Update(sf::Time _frameTime);
+	void Draw(sf::RenderTarget& _target);
+
+private:
+
+	//Assets
+	sf::Sprite m_sprite;
+	sf::Sound m_jumpSound;
+	sf::Sound m_deathSound;
+	sf::Sound m_landSound;
+	AnimationSystem m_animation;
+	//state
+	sf::Vector2 m_velocity
+};
